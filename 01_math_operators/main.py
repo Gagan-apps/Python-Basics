9fd644bc8fdc216e87528f18f92894405cf99318
Gagan-apps <308.08g@gmail.com>
2020-12-24 12:47:41 -0500

    2020-12-24: Math operators

---
+++ 01_math_operators/main.py
@@ -0,0 +1,13 @@
+# a variable holding an ASCII Char
+symbol = '#'
+fill = ' '
+
+length = 10
+
+print(symbol * length)
+print(symbol, end='')
+print(fill * (length-2), end='')
+print(symbol)
+print(symbol * length)
+
+print(symbol + str(1))
